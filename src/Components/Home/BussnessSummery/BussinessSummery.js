1e4d369c8392fca0f16e6b2657fc1374b14acc82
Shafak93 <shafakctg64@gmail.com>
2022-05-22 22:28:16 +0600

    footer compo create and design added

--- src/Components/Home/BussnessSummery/BussinessSummery.js
+++ src/Components/Home/BussnessSummery/BussinessSummery.js
@@ -4,24 +4,29 @@ import CountUp from 'react-countup';
 const BussinessSummery = () => {
     return (
         <div className='p-12'>
-            <h1 className='text-center text-6xl font-bold pb-10'>We have</h1>
+            <h1 className='text-center text-6xl font-bold '>We have</h1>
+            <div class="divider"></div> 
             <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 '>
-            <div>
-            <CountUp className='text-6xl'start={400} end={500} /><span className='text-5xl'>+</span>
-            <h1 className='text-5xl'>Customers</h1>
-            </div>
-            <div>
-            <CountUp className='text-6xl' end={120} /><span className='text-5xl'>M+</span>
-            <h1 className='text-5xl'>Annual Reveneu</h1>
-            </div>
-            <div>
-            <CountUp className='text-6xl' end={33} /><span className='text-5xl'>K+</span>
-            <h1 className='text-5xl'>Reviews</h1>
-            </div>
-            <div>
-            <CountUp className='text-6xl' end={50} /><span className='text-5xl'>+</span>
-            <h1 className='text-5xl'>Tools</h1>
-            </div>
+
+                    <div>
+                    <CountUp className='text-6xl'start={400} end={500} /><span className='text-5xl'>+</span>
+                    <h1 className='text-5xl'>Customers</h1>
+                    </div>
+                    
+                <div>
+                <CountUp className='text-6xl' end={120} /><span className='text-5xl'>M+</span>
+                <h1 className='text-5xl'>Annual Reveneu</h1>
+                </div>
+
+                <div>
+                <CountUp className='text-6xl' end={33} /><span className='text-5xl'>K+</span>
+                <h1 className='text-5xl'>Reviews</h1>
+                </div>
+                
+                <div>
+                <CountUp className='text-6xl' end={50} /><span className='text-5xl'>+</span>
+                <h1 className='text-5xl'>Tools</h1>
+                </div>
         </div>
         </div>
     );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Header/Banner';
 import BussinessSummery from './BussnessSummery/BussinessSummery';
+import Footer from './Footer/Footer';
 import Tools from './Tools/Tools';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Tools></Tools>
             <BussinessSummery></BussinessSummery>
+            <Footer></Footer>
         </div>
     );
 };
