e7ac1261a8b41487d799ed07a44149fc539b3481
Shafak93 <shafakctg64@gmail.com>
2022-05-25 20:35:50 +0600

    AllUsers design added and show data on UI

--- src/Components/Dashboard/AllUsers/AllUsers.js
+++ src/Components/Dashboard/AllUsers/AllUsers.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 
 const AllUsers = () => {
+    const {data : users, isLoading} = useQuery('users', ()=>fetch('http://localhost:5000/user').then(res => res.json()))
+    console.log(users);
+    if(isLoading){
+        return <p>Loading...</p>
+    }
     return (
         <div>
-            <h1>All users</h1>
+            <h1>All users {users.length}</h1>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                  
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Email</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                   {
+                       users.map((user, index) =>
+                        <tr>
+                        <th>{index+1}</th>
+                        <td>{user.email}</td>
+                        <td><button class="btn btn-outline btn-primary">Make Admin</button></td>
+                        <td><button class="btn btn-outline btn-primary">Remove User</button></td>
+                    </tr>
+                        )
+                   }
+                    </tbody>
+                </table>
+                </div>
         </div>
     );
 };
